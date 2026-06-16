b035c1425d0beaaad1ae3d40e1c9f046aa93a9ed
ChaseB-creator <Chasioboxio@gmail.com>
2026-06-15 21:10:45 -0600

    Establishing key configurations through the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
+    println!("Welcome to the Rust Piano!");
+
+    println!("The key configuration is as follows: ");
+    
+    println!("---------------------");
+    println!("Natural Piano Keys: ");
+    println!("C -> A");
+    println!("D -> S");
+    println!("E -> D");
+    println!("F -> F");
+    println!("G -> G");
+    println!("A -> H");
+    println!("B -> J");
+    println!("---------------------");
+    println!("Sharp Piano Keys: ");
+    println!("C# -> W");
+    println!("D# -> E");
+    println!("F# -> T");
+    println!("G# -> Y");
+    println!("A# -> U");
+    println!("A# -> U");
+    println!("B# -> I");
+    println!("---------------------");
 }
