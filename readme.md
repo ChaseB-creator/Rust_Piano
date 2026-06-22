8ae69385aafa55807ff8c088f8ed44a84ee922ee
ChaseB-creator <Chasioboxio@gmail.com>
2026-06-22 10:24:26 -0600

    Finalization of the project

--- readme.md
+++ readme.md
@@ -6,23 +6,26 @@ This is a terminal-based program written in the Rust programming language. Once 
 
 Steps to build and/or run the software:
 
-1. 
+1. You will need to make sure cargo Rust is downloaded and installed on your machine.
 
-2. 
+2. Once have cargo Rust installed, download the zip file for this repo.
+
+3. Then once you have the zip file, extract the zip file, click the 'src' file and open it with your machine's terminal, then run the 'src' file with 'cargo build' & 'cargo run'.
+
+4. Then the program will run inside the user's terminal.
 
 Instructions for using the software:
 
-1. 
-2. 
-3. 
-4. 
+1. The keys: 'a', 's', 'd', 'f', 'g', 'h', and 'j' will play base notes. You tap these keys and base piano notes will play.
+2. The keys: 'w', 'e', 't', 'y', 'u', and 'i' will play sharp notes. 
+3. Because of the threading in the program, multiple notes can be played at the same time.
 
 ## Development Environment
 
 To recreate the development environment, you need the following software and/or libraries with the specified versions:
 
-* 
-* 
+* You will aboslutely need to download and install Rust in order to run this software on your machine. 
+* Once you have Rust installed, all the libraries I used are built-in standard Rust libraries, so if you have Rust running properly then the program should be able to be built and ran with no problems at all. 
 
 ## Useful Websites to Learn More
 
@@ -30,10 +33,14 @@ I found these websites useful in developing this software:
 
 * Reading: [The best place to dive into the Rust programming language and it's documentation](https://rust-lang.org/)
 
+* Reading: [Learn about Rodio and how it's used](https://docs.rs/rodio/latest/rodio/)
+
+* YouTube Video: [Learn how to use Rodio from a video](https://www.youtube.com/watch?v=mXXaXRsguEM)
+
 ## Future Work
 
 The following items I plan to fix, improve, and/or add to this project in the future:
 
-* [ ] 
-* [ ] 
-* [ ]
+* [ ] Adding a octave selection
+* [ ] Assigning more notes to the keys
+* [ ] Sound selection
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,98 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Welcome to the Rust Piano!");
+//Libraries (crates) being used:
+use crossterm::event::{self, Event, KeyCode};
+use rodio::{OutputStream, Sink, source::{SineWave, Source}};
+use std::collections::HashMap;
+use std::time::Duration;
 
-    println!("The key configuration is as follows: ");
-    
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let keyboard_map = build_keyboard_map();
+    print_instructions();
+
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+
+    println!("Press piano keys to play notes. Press Esc or Q to quit.");
+
+    loop {
+        // Checks for if a key was pressed in the last 100 milliseconds.
+        if event::poll(Duration::from_millis(100))? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Char(c) => {
+                        // Turns user input into constant lowercase.
+                        let key = c.to_ascii_lowercase();
+                        if let Some((note_name, freq)) = keyboard_map.get(&key) {
+                            println!("Playing {} ({})", note_name, key);
+                            play_note(*freq, &stream_handle);
+                        } else if key == 'q' {
+                            break;
+                        }
+                    }
+                    KeyCode::Esc => break,
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    println!("Goodbye!");
+    Ok(())
+}
+
+fn build_keyboard_map() -> HashMap<char, (&'static str, f32)> {
+    let mut map = HashMap::new();
+
+    // Assigning keys to notes using a HashMap dictionary.
+    map.insert('a', ("C4", 261.63));
+    map.insert('w', ("C#4", 277.18));
+    map.insert('s', ("D4", 293.66));
+    map.insert('e', ("D#4", 311.13));
+    map.insert('d', ("E4", 329.63));
+    map.insert('f', ("F4", 349.23));
+    map.insert('t', ("F#4", 369.99));
+    map.insert('g', ("G4", 392.00));
+    map.insert('y', ("G#4", 415.30));
+    map.insert('h', ("A4", 440.00));
+    map.insert('u', ("A#4", 466.16));
+    map.insert('j', ("B4", 493.88));
+    map.insert('i', ("C5", 523.25));
+
+    map
+}
+
+fn print_instructions() {
+    println!("Welcome to the Rust Piano!");
+    println!("The key configuration is as follows:");
     println!("---------------------");
-    println!("Natural Piano Keys: ");
-    println!("C -> A");
-    println!("D -> S");
-    println!("E -> D");
-    println!("F -> F");
-    println!("G -> G");
-    println!("A -> H");
-    println!("B -> J");
+    println!("Natural Piano Keys:");
+    println!("C  -> A");
+    println!("D  -> S");
+    println!("E  -> D");
+    println!("F  -> F");
+    println!("G  -> G");
+    println!("A  -> H");
+    println!("B  -> J");
     println!("---------------------");
-    println!("Sharp Piano Keys: ");
+    println!("Sharp Piano Keys:");
     println!("C# -> W");
     println!("D# -> E");
     println!("F# -> T");
     println!("G# -> Y");
     println!("A# -> U");
-    println!("A# -> U");
-    println!("B# -> I");
+    println!("High C -> I");
     println!("---------------------");
+}
+
+fn play_note(frequency: f32, stream_handle: &rodio::OutputStreamHandle) {
+    if let Ok(sink) = Sink::try_new(stream_handle) {
+        let source = SineWave::new(frequency)
+            // Limiting the duration of the note to 400 milliseconds. 
+            .take_duration(Duration::from_millis(400))
+            // Handles the frequency volume.
+            .amplify(0.25);
+        sink.append(source);
+        // Allows for threading, meaning multiple notes can be played at the same time.
+        sink.detach();
+    }
 }
